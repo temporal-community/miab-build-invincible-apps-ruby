a6502e92cb2a4b1e8b46d282abb9f37c656d7a39
angelazhou32 <angela.zhou@temporal.io>
2025-07-09 12:45:12 -0400

    adds meetup in box ruby

---
+++ durable-vs-normal-execution/durable/lib/counter/workflow.rb
@@ -0,0 +1,25 @@
+require 'temporalio/workflow'
+require_relative 'activities'
+
+module Counter
+  class CountingWorkflow < Temporalio::Workflow::Definition
+    def execute
+      i = 1
+      Temporalio::Workflow.logger.warn("***Counting to 10***")
+      
+      while i <= 10
+        Temporalio::Workflow.logger.warn(i.to_s)
+        
+        i = Temporalio::Workflow.execute_activity(
+          AddOneActivity,
+          i,
+          start_to_close_timeout: 60 # maximum time allowed for a single Activity Task Execution
+        )
+        
+        Temporalio::Workflow.sleep(3)
+      end
+      
+      Temporalio::Workflow.logger.warn("***Finished counting to 10***")
+    end
+  end
+end
